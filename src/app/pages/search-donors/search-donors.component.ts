be2514ad9b0ef32c367ffa699a662c81dedb0342
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-09-21 20:46:13 +0600

    filter form added

--- src/app/pages/search-donors/search-donors.component.ts
+++ src/app/pages/search-donors/search-donors.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { InputsComponent } from "../../components/shared/inputs/inputs.component";
+import { FormsModule } from '@angular/forms';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-search-donors',
   standalone: true,
-  imports: [],
+  imports: [InputsComponent, FormsModule],
   templateUrl: './search-donors.component.html',
   styleUrl: './search-donors.component.css'
 })
 export class SearchDonorsComponent {
+  private dataService = inject(DataService);
 
+  model : any;
+  divisions : any;
+  districts : any;
+  bloodGroups : any;
+
+  constructor() {
+    this.model = {
+      division: '',
+      district: '',
+      date: '',
+      bloodGroup: ''
+    };
+  }
+
+  ngOnInit() {
+    this.dataService.getJsonData().subscribe(data => {
+      this.divisions = data?.divisions;
+      this.bloodGroups = data?.bloodGroups;
+    });
+  }
+
+  onFormSubmit(){
+    const {
+      division,
+      district,
+      date,
+      bloodGroup
+    } = this.model;
+      const userInfo = {
+        division,
+        district,
+        date,
+        bloodGroup
+      };
+  }
+
+  onDivisionChanged(){
+    this.dataService.getCityByParentId(this.model.division).subscribe(
+      data => {
+        this.districts = data;
+      },
+      error => {
+        console.error('Error fetching data', error);
+      }
+    );
+  }
 }
