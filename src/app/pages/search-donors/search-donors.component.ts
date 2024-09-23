f85af73b5658088249f4e5133f146293104aa17a
KamrujjamanRony <kamrujjaman0228@gmail.com>
2024-09-23 13:29:12 +0600

    user card added

--- src/app/pages/search-donors/search-donors.component.ts
+++ src/app/pages/search-donors/search-donors.component.ts
@@ -2,21 +2,25 @@ import { Component, inject } from '@angular/core';
 import { InputsComponent } from "../../components/shared/inputs/inputs.component";
 import { FormsModule } from '@angular/forms';
 import { DataService } from '../../services/data.service';
+import { UserCardComponent } from "../../components/shared/user-card/user-card.component";
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-search-donors',
   standalone: true,
-  imports: [InputsComponent, FormsModule],
+  imports: [InputsComponent, FormsModule, UserCardComponent],
   templateUrl: './search-donors.component.html',
   styleUrl: './search-donors.component.css'
 })
 export class SearchDonorsComponent {
   private dataService = inject(DataService);
+  private userService = inject(UserService);
 
   model : any;
   divisions : any;
   districts : any;
   bloodGroups : any;
+  users : any;
 
   constructor() {
     this.model = {
@@ -28,6 +32,10 @@ export class SearchDonorsComponent {
   }
 
   ngOnInit() {
+    this.userService.getAllUsers().subscribe(data => {
+      this.users = data;
+      console.log(this.users)
+    });
     this.dataService.getJsonData().subscribe(data => {
       this.divisions = data?.divisions;
       this.bloodGroups = data?.bloodGroups;
