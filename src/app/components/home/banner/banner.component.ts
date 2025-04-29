eb641fee7edc02782e8bef350d289f69e425cc7c
KamrujjamanRony <kamrujjaman0228@gmail.com>
2025-04-29 21:32:41 +0600

    add mobile app download buttons

--- src/app/components/home/banner/banner.component.ts
+++ src/app/components/home/banner/banner.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { BtnPrimaryComponent } from "../../shared/btn-primary/btn-primary.component";
 import { BtnSecondaryComponent } from "../../shared/btn-secondary/btn-secondary.component";
+import { DataService } from '../../../services/data.service';
 
 @Component({
     selector: 'app-banner',
@@ -9,5 +10,15 @@ import { BtnSecondaryComponent } from "../../shared/btn-secondary/btn-secondary.
     styleUrl: './banner.component.css'
 })
 export class BannerComponent {
+    private dataService = inject(DataService);
+    downloadAndroidAppUrl = signal('');
+    downloadIosAppUrl = signal('');
+
+    ngOnInit() {
+        this.dataService.getJsonData().subscribe(data => {
+            this.downloadAndroidAppUrl.set(data?.download?.android);
+            this.downloadIosAppUrl.set(data?.download?.ios);
+        });
+    }
 
 }
